62f2030d35dfb8163b1b3b226a43db79a37bf928
syed-002 <syedafrin002@gmail.com>
2024-11-24 12:02:52 +0530

    Email Authentication updated

--- src/pages/ViewBookmarks.js
+++ src/pages/ViewBookmarks.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Sidebar from "../components/Sidebar.js";
-import Header from "../components/Header.js";
-import BookGrids from "../components/BookGrids.js";
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import Sidebar from "../components/Sidebar.js";
+// import Header from "../components/Header.js";
+// import BookGrids from "../components/BookGrids.js";
 
-const ViewBookmarks = () => {};
+// const ViewBookmarks = () => {};
 
-export default ViewBookmarks;
+// export default ViewBookmarks;
